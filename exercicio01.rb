4d01c085e27b930da61b3bec03177512163f6718
João Silva <jpsilva.basilio@gmail.com>
2023-05-02 11:04:10 -0300

    add primeiro exercicio

---
+++ exercicio01.rb
@@ -0,0 +1,8 @@
+# Crie um programa que calcule a média aritimética de três números:
+
+puts "Digite três números:"
+num1 = gets.to_f
+num2 = gets.to_f
+num3 = gets.to_f
+media = (num1 + num2 + num3) / 3
+puts "A média é #{media}"
